e033336ba4ab98ace293c811ef2809d0cbd784ad
ranjukr <ranjukr@INranjukr.local>
2015-12-03 12:56:18 +0530

    [Ranju/Anjaly] - Modified the server
    What changes have you made?
    Modified the server.js and made a new route.js
    *
    
    Why do we need this change?
    To handle the requests more easily
    
    *
    
    What can this change impact?
    Easy to understand and process
    
    *

---
+++ routes.js
@@ -0,0 +1,63 @@
+var fs = require('fs');
+var ld = require('lodash');
+var querystring = require('querystring');
+var names = require('./name.js');
+
+var method_not_allowed = function(req, res){
+	res.statusCode = 405;
+	console.log(res.statusCode);
+	res.end('Method is not allowed');
+};
+var fileNotFound = function(req, res){
+	res.statusCode = 404;
+	res.end('Not Found');
+};
+
+var getNames = function(req,res){
+	res.end(JSON.stringify(names.getAll()));
+};
+
+var joinPlayer = function(req, res){
+	var data='';
+	req.on('data',function(chunk){
+    		data+=new Buffer(chunk, 'base64').toString('ascii');
+    	});
+	req.on('end',function(){
+		
+		res.writeHead(301,{
+    		'Location':'/public/html/game.html',
+    		'Content-Type':'text/html',
+    	});
+		var entry = querystring.parse(data);
+		names.add(entry);
+		getNames(req,res);
+	});
+};
+
+var serveIndex = function(req, res, next){
+	req.url = '/public/html/login.html';
+	next();
+};
+var serveStaticFile = function(req, res, next){
+	var filePath = '.' + req.url;
+	fs.readFile(filePath, function(err, data){
+		if(data){
+			res.statusCode = 200;
+			console.log(res.statusCode);
+			res.end(data);
+		}
+		else{
+			next();
+		}
+	});
+};
+
+exports.post_handlers = [
+	{path: '^/$', handler: joinPlayer},
+	{path: '', handler: method_not_allowed}
+];
+exports.get_handlers = [
+	{path: '^/$', handler: serveIndex},
+	{path: '', handler: serveStaticFile},
+	{path: '', handler: fileNotFound}
+];
