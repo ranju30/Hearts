ba7ab36667496f0a746e162c642e41d3fa476cdd
ranjukr <ranjukr@INranjukr.local>
2015-12-05 19:09:34 +0530

    Ranju/Swamiji - added missing login.js changes
    What changes have you made?
    refer to previous commit
    *
    
    Why do we need this change?
    same
    *
    
    What can this change impact?
    same
    *

--- public/javaScript/login.js
+++ public/javaScript/login.js
@@ -1,19 +1,6 @@
-var loginName = function(){
-	var playerName = document.getElementById('user_name').value;
-	console.log("$$$$$",playerName);
-	document.getElementById('user_name').value = '';
-	requestToLogin(user_name);
+var onPageReady = function(){
+	document.cookie = "userName=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 };
+$(document).ready(onPageReady);
 
-
-var requestToLogin = function(userName){
-	var req = new XMLHttpRequest();
-	req.onreadystatechange = function() {
-		console.log(req.readyState);
-	    if (req.readyState == 4 && req.status == 200) {
-	        var comments = JSON.parse(req.responseText);
-	    };
-	};
-	req.open('POST', 'login_user', true);
-	req.send(userName);
-}
+//add styling later
