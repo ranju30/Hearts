e08cd9752e21f966d9f1ce6ff367d18fb5976e4a
ranju <ranjukr@thoughtworks.com>
2016-01-20 10:28:19 +0530

    requested created for passing 3 cards only

--- public/javaScript/game.js
+++ public/javaScript/game.js
@@ -23,20 +23,32 @@ var convertToValueObject = function(cardID){
 	var cardValue = cardID.split(" ");
 	return {suit:cardValue[0],rank:cardValue[1]};
 };
+var passCards = function(){
+	if($('.select').length == 3){
+		$('.action').hide();
+		var selectedCards = [];
+		var firstCard = convertToValueObject($('.select')[0].id);
+		selectedCards.push(firstCard);
+		var secondCard = convertToValueObject($('.select')[1].id);
+		selectedCards.push(secondCard);
+		var thirdCard = convertToValueObject($('.select')[2].id);
+		selectedCards.push(thirdCard);
+		$.post('selectCardToPass','cards='+JSON.stringify(selectedCards));
+		gameStatusTime = timer(); 
+	}
+};
 
 var toggleSelection = function(){
 	$(this).toggleClass('select');
 	if($('.select').length==1){
-		$('.action').show();
 		var selectedCard = convertToValueObject($('.select')[0].id);
 		$.post('startGame',selectedCard);
-	}
-	else
-		$('.action').hide();
+	};
 };
 var bindEvents = function(){
 	$('.card').click(toggleSelection);
 };
+
 var updateBoard = function(data){
 	var getRelativePlayer = function(step){
 		return playerTemplate(data.players[(data.location+step)%4]);
@@ -45,26 +57,28 @@ var updateBoard = function(data){
 		return totalPointTemplate(data.players[(data.location+step)%4]);
 	}
 	$('.status').html(data.instruction);
-
-	$('.totalPoint .name1').html(getTotalPoints(0));
-	$('.totalPoint .name2').html(getTotalPoints(1));
-	$('.totalPoint .name3').html(getTotalPoints(2));
-	$('.totalPoint .name4').html(getTotalPoints(3));
+	if(data.players.length == 4){
+		$('.totalPoint .name1').html(getTotalPoints(0));
+		$('.totalPoint .name2').html(getTotalPoints(1));
+		$('.totalPoint .name3').html(getTotalPoints(2));
+		$('.totalPoint .name4').html(getTotalPoints(3));
+	}
 	
 	$('.playerSelf .name').html(getRelativePlayer(0));
-
 	$('.leftPlayer .name').html(getRelativePlayer(1));
-
 	$('.oppositePlayer .name').html(getRelativePlayer(2));
-
 	$('.rightPlayer .name').html(getRelativePlayer(3));
 
 	$('.playerSelf .hand').html(generateHand(data.hand));
+	
 	bindEvents();
 };
 var finishGame = function(data){
-	$('.status').html(data.winner);
-	clearInterval(gameStatusTime);
+	console.log(data.winner)
+	if(data.winner){
+		$('.status').html(data.winner);
+		clearInterval(gameStatusTime);
+	}
 };
 var checkGameStatus = function(){
 	$.getJSON('gameStatus',updateBoard)
@@ -93,14 +107,12 @@ var timer = function(){
 	setInterval(function(){
 		checkGameStatus();
 		updateRound();
-		$('.action').hide();
 	},1500);
 };
 
 var onPageReady = function(){
-	gameStatusTime = timer();
+	checkGameStatus();
+	updateRound();
 };
 
 $(document).ready(onPageReady);
-
-
